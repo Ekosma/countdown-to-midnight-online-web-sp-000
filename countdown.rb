9632376ff280f2984fd5edc07a59268fb31bf23a
Evelyn Kosma <ezranaja@gmail.com>
2021-02-22 04:01:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,25 @@
 #write your code here
 
-def countdown(x)
-  while x > 0 
-      puts "#{x} SECONDS"
-  x -= 1
-  end
+def countdown(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
 end
 
+
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end	end
